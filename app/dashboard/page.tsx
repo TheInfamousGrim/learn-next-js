0e96d275679e2cfb8070c5ab6eeb81c62a2f95b1
TheInfamousGrim <finchergeorge1@gmail.com>
2023-03-11 00:37:15 +0000

    Feat 🎯: Add navbar and dashboard page
    Experimenting with using next 13 new features such as server routing

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,31 @@
 export default function Dashboard() {
-  return <h1>Hello, this is the dashboard page</h1>
+  return (
+    <div className="relative flex min-h-full flex-col">
+      <div className="mx-auto w-full max-w-7xl flex-grow lg:flex xl:px-8">
+        {/* Left sidebar & main wrapper */}
+        <div className="min-w-0 flex-1 bg-white xl:flex">
+          <div className="border-b border-gray-200 bg-white xl:w-64 xl:flex-shrink-0 xl:border-b-0 xl:border-r xl:border-gray-200">
+            <div className="h-full py-6 pl-4 pr-6 sm:pl-6 lg:pl-8 xl:pl-0">
+              {/* Left column area */}
+              <p>Left column</p>
+            </div>
+          </div>
+
+          <div className="bg-white lg:min-w-0 lg:flex-1">
+            <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
+              {/* Main area */}
+              <p>Main area</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-gray-50 pr-4 sm:pr-6 lg:flex-shrink-0 lg:border-l lg:border-gray-200 lg:pr-8 xl:pr-0">
+          <div className="h-full py-6 pl-6 lg:w-80">
+            {/* Right column area */}
+            <p>Right column</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
